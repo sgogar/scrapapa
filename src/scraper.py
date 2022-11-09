2dacbf56ee6deee8658fb8d8b69af22cb20199c1
FacuGauto <fgauto1992@gmail.com>
2022-11-09 14:24:24 -0300

    bigquery

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from google.cloud import bigquery
 
 def product_getter(urls):
   for url in urls:
@@ -22,20 +23,42 @@ def product_getter(urls):
       productos.append(producto)
   return productos
 
-urls_dia = ["https://diaonline.supermercadosdia.com.ar/papas?_q=papas&map=ft&page=1",
+def create_table_in_bq(productos) -> None:
+    gcp_project = "alumnos-sandbox"
+    table_id = "productos_papa.new_table"
+
+    for producto in productos:
+      df = pd.DataFrame(
+            {
+                "nombre": producto['name'],
+                "marca": producto['marca'],
+                "precio": producto['precio']
+            }
+        )
+    pdbq.to_gbq(df, table_id, project_id=gcp_project, if_exists="append")
+
+
+
+if __name__ == '__main__':
+  urls_dia = ["https://diaonline.supermercadosdia.com.ar/papas?_q=papas&map=ft&page=1",
        "https://diaonline.supermercadosdia.com.ar/papas?_q=papas&map=ft&page=2",
        "https://diaonline.supermercadosdia.com.ar/papas?_q=papas&map=ft&page=3",
        "https://diaonline.supermercadosdia.com.ar/papas?_q=papas&map=ft&page=4"]
 
-print(product_getter(urls_dia))
+  print(product_getter(urls_dia))
 
-urls_vea = ["https://www.vea.com.ar/papas?_q=papas&map=ft",
-       "https://www.vea.com.ar/papas?_q=papas&map=ft&page=2"]
+  #urls_vea = ["https://www.vea.com.ar/papas?_q=papas&map=ft",
+  #     "https://www.vea.com.ar/papas?_q=papas&map=ft&page=2"]
 
-print(product_getter(urls_vea))
+  #print(product_getter(urls_vea))
 
-urls_disco = ["https://www.disco.com.ar/papas?_q=papas&map=ft",
-              "https://www.disco.com.ar/papas?_q=papas&map=ft&page=2",
-              "https://www.disco.com.ar/papas?_q=papas&map=ft&page=3"]
+  #urls_disco = ["https://www.disco.com.ar/papas?_q=papas&map=ft",
+  #            "https://www.disco.com.ar/papas?_q=papas&map=ft&page=2",
+  #            "https://www.disco.com.ar/papas?_q=papas&map=ft&page=3"]
 
-print(product_getter(urls_disco))
+  #print(product_getter(urls_disco))
+  
+  productos = product_getter(urls_dia)
+  
+  # add_data_to_bigquery_and_print_df()
+  create_table_in_bq(productos)
